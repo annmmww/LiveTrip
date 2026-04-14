41c0a9dff933fbba52eeed2f596021aed79cd934
Ahn Yeonjeong <149382038+annmmww@users.noreply.github.com>
2026-04-14 14:44:55 +0900

    feat: 사이드 메뉴에 role을 부여하여 메뉴 전환

--- src/components/side-menu/type.ts
+++ src/components/side-menu/type.ts
@@ -4,11 +4,16 @@ export type MenuItemType =
   | 'manageExperiences'
   | 'reservationStatus';
 
+export type MenuRole = 'user' | 'host' | 'customer';
+
+export type ToggleRole = 'host' | 'customer';
+
 export interface MenuItem {
   id: MenuItemType;
   label: string;
   iconPath: string;
   href: string;
+  role: MenuRole;
 }
 
 export interface SideMenuProps {
