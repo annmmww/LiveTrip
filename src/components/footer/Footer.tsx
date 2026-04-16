9070ab3a38049be7403f291df89a3478a0b9806c
Ahn Yeonjeong <149382038+annmmww@users.noreply.github.com>
2026-04-16 09:10:29 +0900

    chore: 불필요한 공백 제거

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,32 +1,29 @@
 import Link from 'next/link';
 import SocialIcons from '@/components/footer/components/SocialIcons';
 
-;
-
 export default function Footer() {
   const currentYear = new Date().getFullYear();
-  
+
   return (
-      <footer className="w-full border-t border-gray-200 bg-white py-8 px-6 md:py-10 md:px-20 lg:px-50">
-      
-      <div className="grid grid-cols-2 md:grid-cols-3 text-gray-500 gap-4">
+    <footer className='w-full border-t border-gray-200 bg-white px-6 py-8 md:px-20 md:py-10 lg:px-50'>
+      <div className='grid grid-cols-2 gap-4 text-gray-500 md:grid-cols-3'>
         {/* 저작권*/}
-        <div className="md:block text-sm text-gray-400 order-2 md:order-1">
-            &copy;Part4_Team4 - {currentYear}
+        <div className='order-2 text-sm text-gray-400 md:order-1 md:block'>
+          &copy;Part4_Team4 - {currentYear}
         </div>
         {/* 정책 */}
-        <div className="col-span-2 space-x-4 order-1 justify-self-center md:order-2 md:col-span-1">
-          <Link href="/privacy" className="text-gray-600">
+        <div className='order-1 col-span-2 space-x-4 justify-self-center md:order-2 md:col-span-1'>
+          <Link href='/privacy' className='text-gray-600'>
             Privacy Policy
           </Link>
-          <span className="text-gray-600">·</span>
-          <Link href="/faq" className="text-gray-600">
+          <span className='text-gray-600'>·</span>
+          <Link href='/faq' className='text-gray-600'>
             FAQ
           </Link>
         </div>
         {/* 소셜 */}
-        <div className="order-3 justify-self-end">
-            <SocialIcons />
+        <div className='order-3 justify-self-end'>
+          <SocialIcons />
         </div>
       </div>
     </footer>
